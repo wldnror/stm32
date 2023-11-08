94cb586a8e71a2e5cf5db1bbd219cf1b337f7d94
wldnror <72863449+wldnror@users.noreply.github.com>
2023-11-08 19:45:34 +0900

    Update Wi-Fi_scanning.py

--- Wi-Fi_scanning.py
+++ Wi-Fi_scanning.py
@@ -5,13 +5,15 @@ import re
 def scan_wifi_networks(interface="wlan0"):
     cmd = ["sudo", "iwlist", interface, "scan"]
     scan_result = subprocess.run(cmd, capture_output=True, text=True)
-    networks = scan_result.stdout
+    scan_output = scan_result.stdout
 
-    # 정규 표현식을 사용하여 SSID를 추출
-    pattern = re.compile(r'ESSID:"(.*?)"')
-    available_networks = pattern.findall(networks)
+    # 정규 표현식을 사용하여 SSID와 보안 설정을 추출
+    pattern = re.compile(r'Encryption key:off\n\s*ESSID:"([^"]*)"')
 
-    return available_networks
+    # 보안이 해제된 네트워크의 SSID 추출
+    open_networks = pattern.findall(scan_output)
+
+    return open_networks
 
 # 암호화되지 않은 네트워크에 연결하는 함수
 def connect_to_open_network(ssid):
@@ -22,10 +24,14 @@ def connect_to_open_network(ssid):
     if result.returncode == 0:
         print(f"Connected to {ssid}")
     else:
-        print(f"Failed to connect to {ssid}")
+        print(f"Failed to connect to {ssid}. Error: {result.stderr}")
 
 # 메인 로직
 if __name__ == "__main__":
     open_networks = scan_wifi_networks()
-    for ssid in open_networks:
-        connect_to_open_network(ssid)
+    if open_networks:
+        print("Open networks found:", open_networks)
+        for ssid in open_networks:
+            connect_to_open_network(ssid)
+    else:
+        print("No open networks found.")
