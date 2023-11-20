fd943fd14641a1219e418159a360a5686333cf21
wldnror <72863449+wldnror@users.noreply.github.com>
2023-11-21 08:01:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -268,7 +268,7 @@ def update_oled_display():
             draw.text((7, 20), status_message, font=font_status, fill=255)
         else:
             draw.text((0, 51), 'GDSENG', font=font_big, fill=255)
-            draw.text((95, 51), 'ver 2.4', font=font_big, fill=255)
+            draw.text((95, 51), 'ver 2.5', font=font_big, fill=255)
             draw.text((42, 15), f'설정 {current_command_index+1}번', font=font_s, fill=255)
             if command_names[current_command_index] == "ASGD S":
                 draw.text((32, 28), 'ASGD S', font=font, fill=255)
