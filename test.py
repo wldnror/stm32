b4cd16145a6ac1e7eb56bb520654fa714a9dbab2
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-03 20:33:31 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import pygame
 import RPi.GPIO as GPIO
 import spidev
 import time
@@ -7,7 +6,6 @@ import time
 DC_PIN = 24
 RST_PIN = 25
 SPI_CS_PIN = 8
-BLK_PIN = 18  # 백라이트 핀은 선택적입니다.
 
 # SPI 인스턴스 설정
 spi = spidev.SpiDev()
@@ -20,7 +18,6 @@ GPIO.setwarnings(False)
 GPIO.setup(DC_PIN, GPIO.OUT)
 GPIO.setup(RST_PIN, GPIO.OUT)
 GPIO.setup(SPI_CS_PIN, GPIO.OUT)
-GPIO.setup(BLK_PIN, GPIO.OUT)  # 백라이트 핀은 선택적입니다.
 
 # 디스플레이 초기화
 def init_display():
@@ -31,7 +28,6 @@ def init_display():
     time.sleep(0.1)
     GPIO.output(RST_PIN, GPIO.HIGH)
     time.sleep(0.1)
-    # 여기에 초기화 명령을 추가합니다.
 
 # 명령 전송 함수
 def write_command(command):
@@ -47,33 +43,27 @@ def write_data(data):
     spi.writebytes(data)
     GPIO.output(SPI_CS_PIN, GPIO.HIGH)
 
-# 이미지를 디스플레이에 전송하는 함수
-def display_image(image):
-    # 여기에 이미지를 디스플레이에 전송하는 로직을 구현합니다.
+# 색상 표시 함수
+def fill_color(color):
+    width, height = 240, 280  # 디스플레이 해상도
+    pixel_count = width * height
 
-def create_image_with_text(width, height, text):
-    pygame.init()
-    surface = pygame.Surface((width, height))
+    # 명령: 데이터 쓰기 시작
+    write_command(0x2C)
 
-    # 색상 패턴 그리기
-    for y in range(0, height, 40):
-        for x in range(0, width, 40):
-            color = (x % 255, y % 255, (x + y) % 255)
-            pygame.draw.rect(surface, color, (x, y, 40, 40))
+    # 색상 데이터 전송
+    for _ in range(pixel_count):
+        write_data(color)
 
-    # 텍스트 추가
-    font = pygame.font.Font(None, 36)
-    text_surface = font.render(text, True, (255, 255, 255))
-    text_rect = text_surface.get_rect(center=(width/2, height/2))
-    surface.blit(text_surface, text_rect)
-
-    return pygame.image.tostring(surface, 'RGB')
-
+# 메인 함수
 def main():
     init_display()
-    width, height = 240, 280  # 디스플레이 해상도
-    image_data = create_image_with_text(width, height, "Hello, World!")
-    display_image(image_data)
+    GPIO.output(DC_PIN, GPIO.HIGH)  # 데이터 모드로 설정
+
+    # 빨간색(RGB: 255, 0, 0)을 디스플레이에 표시
+    fill_color([0xFF, 0x00, 0x00])
+
+    time.sleep(5)  # 5초 동안 표시 유지
 
 if __name__ == '__main__':
     try:
@@ -82,4 +72,3 @@ if __name__ == '__main__':
         pass
     finally:
         GPIO.cleanup()
-        pygame.quit()
