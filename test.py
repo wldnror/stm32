f336d46a0ea467f125b4a527e8035173e8774352
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-03 19:56:44 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,49 +1,63 @@
 import RPi.GPIO as GPIO
 import spidev
 import time
-# 이전 GPIO 설정 초기화
-GPIO.cleanup()
 
-# ST7789V2 컨트롤러를 위한 기본 설정
-DC = 24 # 데이터/명령 선택 핀
-RST = 25 # 리셋 핀
-SPI_PORT = 0 # SPI 포트
-SPI_DEVICE = 0 # SPI 디바이스
+# 핀 설정
+DC_PIN = 24
+RST_PIN = 25
+SPI_CS_PIN = 8
+BLK_PIN = 18  # 백라이트 핀은 선택적입니다.
 
-# SPI 통신 설정
+# SPI 인스턴스 설정
 spi = spidev.SpiDev()
-spi.open(SPI_PORT, SPI_DEVICE)
-spi.max_speed_hz = 4000000
+spi.open(0, 0)  # SPI 포트 0, 디바이스 0
+spi.max_speed_hz = 20000000  # 20 MHz
 
 # GPIO 설정
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(DC, GPIO.OUT)
-GPIO.setup(RST, GPIO.OUT)
+GPIO.setwarnings(False)
+GPIO.setup(DC_PIN, GPIO.OUT)
+GPIO.setup(RST_PIN, GPIO.OUT)
+GPIO.setup(SPI_CS_PIN, GPIO.OUT)
+GPIO.setup(BLK_PIN, GPIO.OUT)  # 백라이트 핀은 선택적입니다.
 
-# 디스플레이 초기화 함수
+# 디스플레이 초기화
 def init_display():
-    GPIO.output(RST, GPIO.LOW)
+    GPIO.output(SPI_CS_PIN, GPIO.HIGH)
+    GPIO.output(RST_PIN, GPIO.HIGH)
     time.sleep(0.1)
-    GPIO.output(RST, GPIO.HIGH)
+    GPIO.output(RST_PIN, GPIO.LOW)
     time.sleep(0.1)
-    # 초기화 명령 추가 부분
-    # ...
+    GPIO.output(RST_PIN, GPIO.HIGH)
+    time.sleep(0.1)
 
-# 디스플레이에 데이터 쓰기 함수
-def write_data(data):
-    GPIO.output(DC, GPIO.HIGH)
-    spi.xfer([data])
+    # 초기화 명령을 보내야 합니다.
+    # 이 부분은 디스플레이의 데이터시트에 따라 달라집니다.
 
-# 디스플레이에 명령 쓰기 함수
+# 명령 전송 함수
 def write_command(command):
-    GPIO.output(DC, GPIO.LOW)
-    spi.xfer([command])
+    GPIO.output(DC_PIN, GPIO.LOW)
+    GPIO.output(SPI_CS_PIN, GPIO.LOW)
+    spi.writebytes([command])
+    GPIO.output(SPI_CS_PIN, GPIO.HIGH)
 
-# 메인 코드
+# 데이터 전송 함수
+def write_data(data):
+    GPIO.output(DC_PIN, GPIO.HIGH)
+    GPIO.output(SPI_CS_PIN, GPIO.LOW)
+    spi.writebytes([data])
+    GPIO.output(SPI_CS_PIN, GPIO.HIGH)
+
+# 메인 함수
 def main():
     init_display()
-    # 디스플레이에 데이터 또는 명령을 보내서 화면에 내용을 표시
-    # ...
+    # 화면을 초기화한 후에 특정 데이터를 보내거나
+    # 특정 그래픽을 디스플레이에 그리는 코드를 추가합니다.
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        GPIO.cleanup()
