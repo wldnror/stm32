b04d9913d23e51244d47ef12f0c14391bbab35bb
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-05 14:56:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ GPIO.setup(LED_ERROR, GPIO.OUT)
 # 폰트 및 이미지 설정
 font_path = '/usr/share/fonts/truetype/malgun/malgunbd.ttf'
 font_big = ImageFont.truetype(font_path, 11)
-font_s = ImageFont.truetype(font_path, 12)
+font_s = ImageFont.truetype(font_path, 13)
 font_st = ImageFont.truetype(font_path, 11)
 font = ImageFont.truetype(font_path, 17)
 font_status = ImageFont.truetype(font_path, 13)
@@ -312,7 +312,7 @@ def update_oled_display():
             draw.text((7, 20), status_message, font=font_status, fill=255)
         else:
             if command_names[current_command_index] != "시스템 업데이트":
-                draw.text((42, 22), f'설정 {current_command_index+1}번', font=font_s, fill=255)  # 폰트 사이즈 원래대로
+                draw.text((42, 20), f'설정 {current_command_index+1}번', font=font_s, fill=255)  # 폰트 사이즈 원래대로
             if command_names[current_command_index] == "ASGD S":
                 draw.text((30, 35), 'ASGD S', font=font_1, fill=255)
             elif command_names[current_command_index] == "ASGD S PNP":
