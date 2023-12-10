dc5d8e6596e46cc44ac3025d70aedab93585da78
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-10 19:56:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ from adafruit_rgb_display import color565
 import adafruit_rgb_display.st7789 as st7789
 
 # 디스플레이 설정
-cs_pin = digitalio.DigitalInOut(board.CE0) # Chip select (CS) 핀
-dc_pin = digitalio.DigitalInOut(board.D25) # Data/Command (DC) 핀
-reset_pin = digitalio.DigitalInOut(board.D24) # Reset (RST) 핀
+cs_pin = digitalio.DigitalInOut(board.CE0) # Chip select (CS) pin
+dc_pin = digitalio.DigitalInOut(board.D25) # Data/Command (DC) pin
+reset_pin = digitalio.DigitalInOut(board.D24) # Reset (RST) pin
 
-BAUDRATE = 24000000 # SPI 통신 속도
+BAUDRATE = 24000000 # SPI 통신 속도
 
 spi = board.SPI()
 disp = st7789.ST7789(spi, height=280, rotation=180,
-           cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=8000000)
+    cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=8000000)
 
 # 디스플레이 크기에 맞는 이미지 생성
 if disp.rotation % 180 == 90:
-  height = disp.height
-  width = disp.width / 2
+    height = disp.height
+    width = disp.width / 2
 else:
-  width = disp.width
-  height = disp.height
+    width = disp.width
+    height = disp.height
 
 image = Image.new('RGB', (width, height))
 
