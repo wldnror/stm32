b37a21b76900615b27adfbf9db74d6c5bb2c12d5
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-10 17:54:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -335,10 +335,10 @@ def update_oled_display():
             draw.text((95, -1), f"{voltage_percentage:.0f}%", font=font_st, fill=255)
         elif command_names[current_command_index] == "시스템 업데이트":
             draw.text((63, 2), ip_address, font=font_big, fill=255)
-            draw.text((0, 105), 'GDSENG', font=font_big, fill=255)
-            draw.text((80, 105), 'ver 2 .7', font=font_big, fill=255)
-            draw.text((42, 40), f'설정 {current_command_index+1}번', font=font_st, fill=255)  
-        draw.text((-1, 0), current_time, font=font_time, fill=255)
+            draw.text((0, 100), 'GDSENG', font=font_big, fill=255)
+            draw.text((80, 100), 'ver 2 .7', font=font_big, fill=255)
+            draw.text((40, 40), f'설정 {current_command_index+1}번', font=font_s, fill=255)  
+        draw.text((0, 0), current_time, font=font_time, fill=255)
 
         # 사용자 지정 위치와 폰트 크기로 메시지 표시
         if status_message:
