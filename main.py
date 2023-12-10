110a690e6bce1016ce3fd7f1eed180fa4b2fa1cd
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-10 19:21:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import RPi.GPIO as GPIO
+from PIL import Image, ImageDraw, ImageFont
 from luma.core.interface.serial import spi
-from luma.lcd.device import st7789
 from luma.core.render import canvas
-from PIL import ImageDraw, ImageFont
+from luma.lcd.device import st7789
 
-# SPI 설정 및 ST7789 디스플레이 초기화
-# gpio_DC와 gpio_RST는 Raspberry Pi의 해당 GPIO 핀 번호로 변경하세요.
-serial = spi(port=0, device=0, gpio_DC=23, gpio_RST=24)
-device = st7789(serial, rotate=0, width=240, height=240)
+try:
+    # GPIO 핀 번호 설정 (Raspberry Pi에 따라 변경해야 할 수 있음)
+    RST_PIN = 25  # 예시로 25번 핀 사용
+    DC_PIN = 24   # 예시로 24번 핀 사용
 
-# 화면에 텍스트 출력
-with canvas(device) as draw:
-    # 폰트 설정 (기본 폰트 사용)
-    font = ImageFont.load_default()
-    
-    # "Hello, World!" 텍스트 출력
-    draw.text((10, 10), "Hello, World!", font=font, fill="white")
+    # SPI 인터페이스와 ST7789 디스플레이 초기화
+    serial = spi(port=0, device=0, gpio_DC=DC_PIN, gpio_RST=RST_PIN)
+    device = st7789(serial, rotate=0, width=240, height=280)
 
-# GPIO 정리
-GPIO.cleanup()
+    # 화면에 텍스트 출력
+    with canvas(device) as draw:
+        font = ImageFont.load_default()
+        draw.text((10, 10), "Hello, World!", font=font, fill="white")
+
+finally:
+    # GPIO 리소스 정리
+    GPIO.cleanup()
