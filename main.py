f93076bd10d5e75a395a36da6b622c101fe69d2d
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-10 17:42:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -336,9 +336,9 @@ def update_oled_display():
         elif command_names[current_command_index] == "시스템 업데이트":
             draw.text((63, 0), ip_address, font=font_big, fill=255)
             draw.text((0, 110), 'GDSENG', font=font_big, fill=255)
-            draw.text((90, 110), 'ver 2 .7', font=font_big, fill=255)
+            draw.text((80, 110), 'ver 2 .7', font=font_big, fill=255)
             draw.text((42, 40), f'설정 {current_command_index+1}번', font=font_st, fill=255)  
-        draw.text((-1, -3), current_time, font=font_time, fill=255)
+        draw.text((0, 0), current_time, font=font_time, fill=255)
 
         # 사용자 지정 위치와 폰트 크기로 메시지 표시
         if status_message:
