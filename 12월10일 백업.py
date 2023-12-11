0734029cccce90f947a88b955677421cabc48a7e
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-11 21:18:24 +0900

    Update 12월10일 백업.py

--- 12월10일 백업.py
+++ 12월10일 백업.py
@@ -330,11 +330,11 @@ def update_oled_display():
         if command_names[current_command_index] in ["ASGD S", "ASGD S PNP"]:
             battery_icon = select_battery_icon(voltage_percentage)
             draw.bitmap((0, 0), mode_icon, fill=255)
-            draw.bitmap((90, -12), battery_icon, fill=255)
+            draw.bitmap((90, -14), battery_icon, fill=255)
             draw.text((99, 0), f"{voltage_percentage:.0f}%", font=font_st, fill=255)
         elif command_names[current_command_index] == "시스템 업데이트":
             draw.text((63, 0), ip_address, font=font_big, fill=255)
-            draw.text((0, 51), 'GDSENG', font=font_big, fill=255)
+            draw.text((0, 53), 'GDSENG', font=font_big, fill=255)
             draw.text((90, 50), 'ver 2 .7', font=font_big, fill=255)
             draw.text((42, 15), f'설정 {current_command_index+1}번', font=font_st, fill=255)  
         draw.text((0, -3), current_time, font=font_time, fill=255)
