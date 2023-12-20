c636a68dc58a5f685f3fdac55e270f2c32c04491
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-20 23:13:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -372,7 +372,7 @@ def update_oled_display():
             elif command_names[current_command_index] == "ASGD S PNP":
                 draw.text((1, 27), 'ASGD S PNP', font=font_1, fill=255)
             elif command_names[current_command_index] == "시스템 업데이트":
-                draw.text((1, 27), '시스템 업데이트', font=font, fill=255)
+                draw.text((1, 20), '시스템 업데이트', font=font, fill=255)
 
 def get_ip_address():
     try:
