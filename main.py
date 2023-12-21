b7d638843bbf05ba0411c98d9714f00610eb4d94
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-21 11:11:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -37,10 +37,6 @@ def toggle_mode():
     is_auto_mode = not is_auto_mode
     update_oled_display()  # OLED 화면 업데이트
 
-# 자동 모드와 수동 모드 아이콘 로드
-# auto_mode_icon = Image.open("/home/user/stm32/img/A.png")
-# manual_mode_icon = Image.open("/home/user/stm32/img/M.png")
-
 # 자동 모드와 수동 모드 아이콘 대신 문자열 사용
 auto_mode_text = 'A'
 manual_mode_text = 'M'
@@ -449,38 +445,3 @@ try:
 
 except KeyboardInterrupt:
     GPIO.cleanup()
-
-# try:
-#     while True:
-#         # 배터리 수준을 확인하고 0%면 시스템 종료
-#         if read_ina219_percentage() == 0:
-#             print("배터리 수준이 0%입니다. 시스템을 종료합니다.")
-#             shutdown_system()
-
-#         # STM32 연결 상태 확인 및 명령 실행
-#         if is_auto_mode and check_stm32_connection() and connection_success:
-#             execute_command(current_command_index)
-
-#         # 두 버튼을 동시에 눌렀을 때 모드 전환
-#         if not GPIO.input(BUTTON_PIN_NEXT) and not GPIO.input(BUTTON_PIN_EXECUTE):
-#             toggle_mode()
-#             time.sleep(1)  # 디바운싱을 위한 지연
-
-#         # NEXT 버튼 처리
-#         elif not GPIO.input(BUTTON_PIN_NEXT):
-#             current_command_index = (current_command_index + 1) % len(commands)
-#             time.sleep(0.1)
-
-#         # EXECUTE 버튼 처리
-#         elif not GPIO.input(BUTTON_PIN_EXECUTE):
-#             execute_command(current_command_index)
-#             time.sleep(0.1)
-
-#         # OLED 디스플레이 업데이트
-#         update_oled_display()
-
-#         # 짧은 지연
-#         time.sleep(0.1)
-
-# except KeyboardInterrupt:
-#     GPIO.cleanup()
