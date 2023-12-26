df28176df0dbba2b32919eb6fa4a2b28a8b909ac
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-26 09:12:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -369,10 +369,10 @@ def execute_command(command_index):
         return
 
     GPIO.output(LED_DEBUGGING, True)
-    display_progress_and_message(0, "업데이트 중...", message_position=(17, 20), font_size=15)
+    display_progress_and_message(0, "업데이트 중...", message_position=(15, 10), font_size=15)
     process = subprocess.Popen(commands[command_index], shell=True)
     while process.poll() is None:
-        display_progress_and_message(50, "업데이트 중...", message_position=(17, 20), font_size=15)
+        display_progress_and_message(50, "업데이트 중...", message_position=(15, 10), font_size=15)
         time.sleep(1)
     result = process.returncode
     GPIO.output(LED_DEBUGGING, False)
@@ -380,7 +380,7 @@ def execute_command(command_index):
     if result == 0:
         print(f"'{commands[command_index]}' 업데이트 성공!")
         GPIO.output(LED_SUCCESS, True)
-        display_progress_and_message(100, "업데이트 성공!", message_position=(15, 20), font_size=15)
+        display_progress_and_message(100, "업데이트 성공!", message_position=(10, 10), font_size=15)
         # display_progress_bar(100)
         time.sleep(1)
         GPIO.output(LED_SUCCESS, False)
@@ -388,7 +388,7 @@ def execute_command(command_index):
     else:
         print(f"'{commands[command_index]}' 업데이트 실패!")
         GPIO.output(LED_ERROR, True)
-        display_progress_and_message(50,"업데이트 실패", message_position=(15, 20), font_size=15)
+        display_progress_and_message(50,"업데이트 실패", message_position=(10, 10), font_size=15)
         # display_progress_bar(50)
         time.sleep(1)
         GPIO.output(LED_ERROR, False)
