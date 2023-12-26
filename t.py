a299610869c70cce4cd59349a97a0a079a00681f
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-26 12:06:42 +0900

    Update t.py

--- t.py
+++ t.py
@@ -1,28 +1,25 @@
 import subprocess
 
 def extract_firmware():
-    firmware_path = "/home/pi/stm32_firmware.bin"  # 추출된 펌웨어를 저장할 경로
-    address = "0x08000000"  # 시작 메모리 주소
-    width = "32"  # 읽을 데이터의 너비 (32비트)
-    count = "0x10000"  # 읽을 데이터의 양
+    start_address = "0x08000000"  # 플래시 메모리 시작 주소
+    length = "0x40000"  # 256KB (STM32F103RCT6 모델 기준)
 
     openocd_command = [
-        "sudo", "openocd",
+        "openocd",
         "-f", "/usr/local/share/openocd/scripts/interface/raspberrypi-native.cfg",
         "-f", "/usr/local/share/openocd/scripts/target/stm32f1x.cfg",
-        "-c", f"init; reset halt; mdw {address} {count} {width}; reset run; shutdown"
+        "-c", f"init; mdw {start_address} {length}; exit"
     ]
 
     try:
-        result = subprocess.run(openocd_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        if result.returncode == 0:
-            with open(firmware_path, 'w') as file:
-                file.write(result.stdout)  # 추출된 데이터를 파일로 저장
-            print("펌웨어 추출 성공:", firmware_path)
+        result = subprocess.run(openocd_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+        if result.stdout:
+            with open("firmware_dump.bin", "w") as file:
+                file.write(result.stdout)
+            print("펌웨어 추출 성공")
         else:
-            print("펌웨어 추출 실패:", result.stderr)
-    except Exception as e:
-        print("펌웨어 추출 중 오류 발생:", str(e))
+            print("펌웨어 추출 실패: 응답 없음")
+    except subprocess.CalledProcessError as e:
+        print(f"펌웨어 추출 실패: {e.stderr}")
 
 extract_firmware()
-
