5d742f220b6d08ee7ab68e88908b625c47a7b280
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-29 01:50:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -48,16 +48,16 @@ def button_next_callback(channel):
 
 def button_execute_callback(channel):
     global current_command_index, need_update
-    # NEXT 버튼도 눌려있는지 확인
-    if not GPIO.input(BUTTON_PIN_NEXT):
-        toggle_mode()  # 모드 전환
-        need_update = True
+    # 시스템 업데이트 화면에서 선택 버튼으로 작동
+    if current_command_index == command_names.index("시스템 업데이트"):
+        execute_command(current_command_index)
     else:
+        # 다른 화면에서의 기능
         if is_auto_mode:
             current_command_index = (current_command_index - 1) % len(commands)
         else:
             execute_command(current_command_index)
-        need_update = True
+    need_update = True
 
     
 
