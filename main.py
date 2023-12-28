7c7d470d55f9ed9f4638d7a4a8e3b6cb47f40d28
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-28 19:52:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -453,17 +453,17 @@ try:
         # 두 버튼을 동시에 눌렀을 때 모드 전환
         if not GPIO.input(BUTTON_PIN_NEXT) and not GPIO.input(BUTTON_PIN_EXECUTE):
             toggle_mode()
-            time.sleep(1)  # 디바운싱을 위한 지연
+            time.sleep(0.05)  # 디바운싱을 위한 지연
 
         # NEXT 버튼 처리
         elif not GPIO.input(BUTTON_PIN_NEXT):
             current_command_index = (current_command_index + 1) % len(commands)
-            time.sleep(0.1)
+            time.sleep(0.05)
 
         # EXECUTE 버튼 처리
         elif not GPIO.input(BUTTON_PIN_EXECUTE):
             execute_command(current_command_index)
-            time.sleep(0.1)
+            time.sleep(0.05)
 
         # OLED 디스플레이 업데이트
         update_oled_display()
