548f23c35e8be9f9e7d2edf5643e1e083a972d96
wldnror <72863449+wldnror@users.noreply.github.com>
2023-12-28 23:38:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -339,14 +339,14 @@ def execute_command(command_index):
         GPIO.output(LED_ERROR1, False)
         return
 
-    GPIO.output(LED_DEBUGGING, True)
+    # GPIO.output(LED_DEBUGGING, True)
     display_progress_and_message(0, "업데이트 중...", message_position=(12, 10), font_size=15)
     process = subprocess.Popen(commands[command_index], shell=True)
     while process.poll() is None:
         display_progress_and_message(50, "업데이트 중...", message_position=(12, 10), font_size=15)
         time.sleep(1)
     result = process.returncode
-    GPIO.output(LED_DEBUGGING, False)
+    # GPIO.output(LED_DEBUGGING, False)
     # display_progress_bar(50)
     if result == 0:
         print(f"'{commands[command_index]}'업데이트 성공!")
