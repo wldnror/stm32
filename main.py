8b4096861938c057f60f3e3b37d9d5aa2fc13575
wldnror <72863449+wldnror@users.noreply.github.com>
2024-01-01 22:54:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ serve_script = "/home/user/stm32/serve.py"
 error_script = "/home/user/stm32/error.py"
 
 try:
-    result = subprocess.run(["python3", serve_script], check=True)
+    result = subprocess.run(["python3", serve_script], check=True, timeout=10)
 except subprocess.CalledProcessError:
     # serve.py 실행 실패
+    # error.py 실행
+    subprocess.run(["python3", error_script])
+except subprocess.TimeoutExpired:
+    # serve.py 실행이 10초 동안 완료되지 않았을 때 처리할 내용
     # error.py 실행
     subprocess.run(["python3", error_script])
