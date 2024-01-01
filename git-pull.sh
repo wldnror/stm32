9e4f02a62296f59b75c841c24f8a727976645f5d
wldnror <72863449+wldnror@users.noreply.github.com>
2024-01-01 18:23:39 +0900

    Update git-pull.sh

--- git-pull.sh
+++ git-pull.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 cd /home/user/stm32
-# git stash
+git fetch origin
+
+local=$(git rev-parse @)
+remote=$(git rev-parse @{u})
+
+if [ $local = $remote ]; then
+    echo "이미 최신 상태입니다."
+    exit 0
+fi
+
+git stash
 git pull
-# git stash pop
+git stash pop
