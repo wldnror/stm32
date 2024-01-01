60e6c33edc9508a1d8a9a52e286b6e5a1dc7f047
wldnror <72863449+wldnror@users.noreply.github.com>
2024-01-01 22:23:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 import subprocess
 
-max_retries = 3  # 최대 재시도 횟수 설정
-serve_script = "/home/user/stm32/serve.py"
-error_script = "/home/user/stm32/error.py"
-
-for attempt in range(max_retries):
-    try:
-        # serve.py 실행
-        result = subprocess.run(["python3", serve_script], check=True)
-        break  # 성공 시 루프 종료
-    except subprocess.CalledProcessError:
-        # serve.py 실행 실패
-        if attempt == max_retries - 1:  # 마지막 시도에서도 실패한 경우
-            # error.py 실행
-            subprocess.run(["python3", error_script])
-        # 그 외 경우는 다시 시도
+try:
+    # serve.py 실행
+    result = subprocess.run(["python3", "serve.py"], check=True)
+except subprocess.CalledProcessError:
+    # serve.py 실행 중 오류 발생 시 error.py 실행
+    subprocess.run(["python3", "error.py"])
