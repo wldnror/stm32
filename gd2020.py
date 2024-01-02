78b6159486e0b8bdbde740e749ce67168c8bbc61
wldnror <72863449+wldnror@users.noreply.github.com>
2024-01-02 18:53:23 +0900

    Update gd2020.py

--- gd2020.py
+++ gd2020.py
@@ -1,28 +1,21 @@
-# import subprocess
-# import os
-
-# # 프로그래밍할 HEX 파일의 경로
-# hex_file_path = '/home/user/stm32/Program/nh3-gn8020-e.hex'
-
-# # 파일의 읽기/쓰기 권한 변경
-# os.chmod(hex_file_path, 0o666)
-
-# # Pickle 명령어를 사용하여 PIC 프로그래밍
-# try:
-#     subprocess.run(['pickle', 'p14', 'lvp', 'program', hex_file_path], check=True)
-#     print("프로그래밍 성공")
-# except subprocess.CalledProcessError as e:
-#     print(f"프로그래밍 실패: {e}")
 
 import RPi.GPIO as GPIO
 import time
+
+# 기존 설정 정리
+GPIO.cleanup()
+
+# GPIO 설정
+PGD_PIN = 11
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PGD_PIN, GPIO.OUT)
+
 
 # GPIO 핀 설정
 PGC_PIN = 10
 PGD_PIN = 11
 # MCLR_PIN = 5V  # 이 핀은 실제 GPIO 핀이 아니므로, 라즈베리 파이에서 제어할 수 없습니다.
 
-GPIO.setmode(GPIO.BCM)
 GPIO.setup(PGC_PIN, GPIO.OUT)
 GPIO.setup(PGD_PIN, GPIO.OUT)
 # MCLR 핀은 별도의 전원 공급 장치를 통해 제어되어야 합니다.
@@ -52,4 +45,19 @@ exit_programming_mode()
 
 GPIO.cleanup()
 
+# import subprocess
+# import os
+
+# # 프로그래밍할 HEX 파일의 경로
+# hex_file_path = '/home/user/stm32/Program/nh3-gn8020-e.hex'
+
+# # 파일의 읽기/쓰기 권한 변경
+# os.chmod(hex_file_path, 0o666)
+
+# # Pickle 명령어를 사용하여 PIC 프로그래밍
+# try:
+#     subprocess.run(['pickle', 'p14', 'lvp', 'program', hex_file_path], check=True)
+#     print("프로그래밍 성공")
+# except subprocess.CalledProcessError as e:
+#     print(f"프로그래밍 실패: {e}")
 
