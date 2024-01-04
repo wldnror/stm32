b51f8556b5b583f6bcc8f95e9906d333bb43944b
wldnror <72863449+wldnror@users.noreply.github.com>
2024-01-04 16:15:28 +0900

    Update serve.py

--- serve.py
+++ serve.py
@@ -521,7 +521,7 @@ def update_oled_display():
                 elif command_names[current_command_index] == "IPA":
                     draw.text((47, 27), 'IPA', font=font_1, fill=255)
                 elif command_names[current_command_index] == "ASGD S PNP":
-                    draw.text((1, 27), 'ASGD S PNP', font=font_1, fill=255)
+                    draw.text((2, 27), 'ASGD S PNP', font=font_1, fill=255)
                 elif command_names[current_command_index] == "시스템 업데이트":
                     draw.text((1, 20), '시스템 업데이트', font=font, fill=255)
 
