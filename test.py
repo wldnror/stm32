2847f4287fb4a09a3c9fdea0c583bcff2407c3af
wldnror <72863449+wldnror@users.noreply.github.com>
2024-05-30 16:17:50 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import Adafruit_ADS1x15
 
-# ADS1115 객체 생성, I2C 주소 설정 (예: 0x49)
-adc = Adafruit_ADS1x15.ADS1115(address=0x49)
+# ADS1115 객체 생성, I2C 주소 설정 (0x48로 설정)
+adc = Adafruit_ADS1x15.ADS1115(address=0x48)
 
 # Gain 설정 (1은 +/- 4.096V 범위)
 GAIN = 1
 
-# 채널 0에서 데이터 읽기 (단일 종료 모드)
-value = adc.read_adc(0, gain=GAIN)
-print('Channel 0: {}'.format(value))
+try:
+    # 채널 0에서 데이터 읽기 (단일 종료 모드)
+    value = adc.read_adc(0, gain=GAIN)
+    print('Channel 0: {}'.format(value))
+except OSError as e:
+    print('I2C 통신 오류: {}'.format(e))
