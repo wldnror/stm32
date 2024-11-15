cd223efc90d78b2535c8fe7f616d108a38f52a58
용굴이 <72863449+wldnror@users.noreply.github.com>
2024-11-15 10:45:48 +0900

    Update serve.py

--- serve.py
+++ serve.py
@@ -34,10 +34,6 @@ root = tk.Tk()
 root.title("업데이트 관리자")
 root.geometry("500x300")  # 필요에 따라 크기 조정
 
-# GUI 요소 정의
-mode_label = tk.Label(root, text="모드: 자동", font=("Helvetica", 16))
-mode_label.pack(pady=10)
-
 current_command_label = tk.Label(root, text=f"현재 명령어: {command_names[current_command_index]}", font=("Helvetica", 14))
 current_command_label.pack(pady=5)
 
