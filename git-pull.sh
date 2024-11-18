9dc26c819142baa2b062178ff02bfff03cc3eab9
용굴이 <72863449+wldnror@users.noreply.github.com>
2024-11-18 13:37:44 +0900

    Update git-pull.sh

--- git-pull.sh
+++ git-pull.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
-LOGFILE="/home/user/stm32/git-pull.log"
-echo "===== $(date) =====" >> $LOGFILE
-cd /home/user/stm32 >> $LOGFILE 2>&1
+cd /home/user/stm32
+git fetch origin
 
-# 원격 저장소 정보 업데이트
-git remote update >> $LOGFILE 2>&1
+local=$(git rev-parse @)
+remote=$(git rev-parse @{u})
 
-# 브랜치 상태 확인
-if git status -uno | grep -q 'Your branch is up to date'; then
-    echo "이미 최신 상태입니다." >> $LOGFILE
+if [ $local = $remote ]; then
+    echo "이미 최신 상태입니다."
     exit 0
 fi
 
-# 변경사항이 있을 경우 임시로 저장
-git stash >> $LOGFILE 2>&1
-
-# 업데이트 적용
-git pull >> $LOGFILE 2>&1
-
-# 임시로 저장한 변경사항 다시 적용
-git stash pop >> $LOGFILE 2>&1
-
-echo "업데이트 완료." >> $LOGFILE
+# git stash
+git pull
+# git stash pop
