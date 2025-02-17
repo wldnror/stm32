03cbe14de6e755ed49a7b8ef7c9e4ac68cbb2988
용굴이 <72863449+wldnror@users.noreply.github.com>
2025-02-17 11:42:51 +0900

    Update serve.py

--- serve.py
+++ serve.py
@@ -226,12 +226,12 @@ commands = [
     "sudo openocd -f /usr/local/share/openocd/scripts/interface/raspberrypi-native.cfg -f /usr/local/share/openocd/scripts/target/stm32f1x.cfg -c \"program /home/user/stm32/Program/HC100.bin verify reset exit 0x08000000\"",
     "sudo openocd -f /usr/local/share/openocd/scripts/interface/raspberrypi-native.cfg -f /usr/local/share/openocd/scripts/target/stm32f1x.cfg -c \"program /home/user/stm32/Program/SAT4010.bin verify reset exit 0x08000000\"",
     "sudo openocd -f /usr/local/share/openocd/scripts/interface/raspberrypi-native.cfg -f /usr/local/share/openocd/scripts/target/stm32f1x.cfg -c \"program /home/user/stm32/Program/IPA.bin verify reset exit 0x08000000\"",
-    "sudo openocd -f /usr/local/share/openocd/scripts/interface/raspberrypi-native.cfg -f /usr/local/share/openocd/scripts/target/stm32f1x.cfg -c \"program /home/user/stm32/Program/v355.bin verify reset exit 0x08000000\"",
-    "sudo openocd -f /usr/local/share/openocd/scripts/interface/raspberrypi-native.cfg -f /usr/local/share/openocd/scripts/target/stm32f1x.cfg -c \"program /home/user/stm32/Program/v355PNP.bin verify reset exit 0x08000000\"",
+    "sudo openocd -f /usr/local/share/openocd/scripts/interface/raspberrypi-native.cfg -f /usr/local/share/openocd/scripts/target/stm32f1x.cfg -c \"program /home/user/stm32/Program/V356.bin verify reset exit 0x08000000\"",
+    "sudo openocd -f /usr/local/share/openocd/scripts/interface/raspberrypi-native.cfg -f /usr/local/share/openocd/scripts/target/stm32f1x.cfg -c \"program /home/user/stm32/Program/V356_PNP.bin verify reset exit 0x08000000\"",
     "git_pull",  # 이 함수는 나중에 execute_command 함수에서 호출됩니다.
 ]
 
-command_names = ["ORG","HMDS","HMDS-IR","ARF-T","HC100","SAT4010","IPA","v355","v355PNP","시스템 업데이트"]
+command_names = ["ORG","HMDS","HMDS-IR","ARF-T","HC100","SAT4010","IPA","V356","V356_PNP","시스템 업데이트"]
 
 current_command_index = 0
 status_message = ""
@@ -488,7 +488,7 @@ def update_oled_display():
                 draw.ellipse(outer_ellipse_box, outline="white", fill=None)
                 draw.text(text_position[mode_char], mode_char, font=font, fill=255)
 
-            if command_names[current_command_index] in ["ORG","HMDS","HMDS-IR","ARF-T","HC100","SAT4010","IPA","v355","v355PNP"]:
+            if command_names[current_command_index] in ["ORG","HMDS","HMDS-IR","ARF-T","HC100","SAT4010","IPA","V356","V356_PNP"]:
                 battery_icon = select_battery_icon(voltage_percentage)
                 draw.bitmap((90, -9), battery_icon, fill=255)
                 draw.text((99, 3), f"{voltage_percentage:.0f}%", font=font_st, fill=255)
@@ -518,9 +518,9 @@ def update_oled_display():
                     draw.text((22, 27), 'SAT4010', font=font_1, fill=255)
                 elif command_names[current_command_index] == "IPA":
                     draw.text((46, 27), 'IPA', font=font_1, fill=255)
-                elif command_names[current_command_index] == "v355":
+                elif command_names[current_command_index] == "V356":
                     draw.text((33, 27), 'v355', font=font_1, fill=255)
-                elif command_names[current_command_index] == "v355PNP":
+                elif command_names[current_command_index] == "V356_PNP":
                     draw.text((22, 27), 'v355PNP', font=font_1, fill=255)
                 elif command_names[current_command_index] == "시스템 업데이트":
                     draw.text((1, 20), '시스템 업데이트', font=font, fill=255)
